a1217f03e9d4f92ea1625d9657db25be0882f17f
Drikus Roor <drikusroor@gmail.com>
2020-09-30 18:30:36 +0200

    Initial commit

---
+++ Assets/WebcamStreamer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WebcamStreamer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        WebCamTexture webcamTexture = new WebCamTexture();
+        Renderer renderer = GetComponent<Renderer>();
+        renderer.material.mainTexture = webcamTexture;
+        webcamTexture.Play();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
